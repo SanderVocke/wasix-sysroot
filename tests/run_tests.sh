b8f4fa38a06972ca0b991a5e75635c178ba1de32
Sander Vocke <sandervocke@gmail.com>
2023-09-06 22:59:57 +0200

    WIP

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,13 +8,21 @@ PASSED_TESTS=""
 if [ ! -z "${@:1}" ]; then
     TESTS=${@:1}
 else
-    TESTS=$(cd $SCRIPT_DIR && ls -d */)
+    TESTS=$(cd $SCRIPT_DIR && ls -d */ | tr -d '/')
 fi
 
 for test in $TESTS; do
     mkdir -p $SCRIPT_DIR/../test-builds/${test}
     cd $SCRIPT_DIR/../test-builds/${test}
-    cmake --fresh -DCMAKE_MAKE_PROGRAM=make -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN -DCMAKE_SYSROOT=/opt/wasix-sysroot $SCRIPT_DIR/${test}
+    cmake \
+      --fresh \
+      -DCMAKE_MAKE_PROGRAM=make \
+      -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
+      -DCMAKE_SYSROOT=/opt/wasix-sysroot \
+      -DCMAKE_C_FLAGS="-I/opt/wasix-sysroot/include -L/opt/wasix-sysroot/lib" \
+      -DCMAKE_CXX_FLAGS="-I/opt/wasix-sysroot/include -L/opt/wasix-sysroot/lib" \
+      -DCMAKE_EXE_LINKER_FLAGS="-L/opt/wasix-sysroot/lib" \
+      $SCRIPT_DIR/${test}
     cmake --build . --target all
     bash $SCRIPT_DIR/${test}/test.sh
     RESULT=$?
